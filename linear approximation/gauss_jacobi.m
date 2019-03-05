d352be79ed9f53f17d170658371ea4e64f0ab73d
dodonut <viniciusvims@gmail.com>
2019-03-05 22:17:38 +0000

    done bisection

--- linear approximation/gauss_jacobi.m
+++ linear approximation/gauss_jacobi.m
@@ -1,10 +1,10 @@
 % Gauss-jacobi method
 
-function [x] = gauss_jacobi(A,b,abs_error, itermax)
-  %GAUSS_JACOBI(A,b,abs_error,itermax)
+function [x] = gauss_jacobi(A,b,relative_error, itermax)
+  %GAUSS_JACOBI(A,b,relative_error,itermax)
   %A            Coeficients matrix
   %b            Column vector of constants
-  %abs_error    Precision which make the algorithm to stop
+  %relative_error    Precision which make the algorithm to stop
   %itermax      Max number of iterations
   
   x = zeros(size(b)); %instantiate the results and also make a guess
@@ -14,7 +14,7 @@ function [x] = gauss_jacobi(A,b,abs_error, itermax)
     for i = 1:n       % run throught each row
       x(i) = (b(i) - A(i,[1:i-1,i+1:n])*x0([1:i-1,i+1:n]))/A(i,i); %isolate the x(i)
     endfor
-    if norm(x-x0,'inf')<abs_error   %calculates the norm of the vector
+    if norm(x-x0,'inf')<relative_error   %calculates the norm of the vector
       printf("Results converge at %d iteration\n",k);
       return
     end
---
+++ non-linear approximation/bisection.m
@@ -0,0 +1,34 @@
+function [p] = bisection(Func, a, b, abs_error, itermax)
+  %BISECTION(Func, a, b, relative_error, itermax)
+  %Func               non-linear function
+  %a,b                interval with f(a) and f(b) with oposite sings
+  %abs_error          Precision which make the algorithm to stop
+  %itermax            Max number of iterations
+  if a == b
+    error('It must be given 2 different points');
+  endif
+  if (Func(a) > 0 && Func(b) > 0) || (Func(a) < 0 && Func(b) < 0)
+    error('Interval do not cross the x-axis');
+  endif   
+  if Func(a) > Func(b)
+    temp = a;
+    a = b;
+    b = temp;
+  endif
+  for k = 1:itermax
+    fa = Func(a);
+    fb = Func(b);
+    p = (a+b)/2;
+    fp = Func(p);
+    if abs(fp) < abs_error
+      printf("Found solution with %d iterations.\n",k);
+      return;
+    endif
+    if fp < 0
+      a = p;
+    else
+      b = p;
+    endif
+  endfor
+  error("Could not find solution within %d iterations\n");
+endfunction
