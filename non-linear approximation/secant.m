7ffe3b56b0c963feb8b6d6b894897870aed8b852
dodonut <viniciusvims@gmail.com>
2019-03-06 23:27:05 +0000

    done secant

---
+++ non-linear approximation/secant.m
@@ -0,0 +1,19 @@
+function [x,y] = secant(Func, a,b, tol, itermax)
+  %SECANT(Func, a, b, relative_error, itermax)
+  %Func               non-linear function
+  %a,b                interval with f(a) and f(b) with oposite sings
+  %tol                Error tolerance which make the algorithm to stop
+  %itermax            Max number of iterations
+  for k = 1:itermax
+    fa = Func(a);
+    fb = Func(b);
+    x = (b*fa - a*fb)/(fa-fb);
+    if abs(Func(x)) < tol
+      printf("Found solution with %d iterations.\n",k);
+      return;
+    endif
+    b = a;
+    a = x;
+  endfor
+  error("Could not find solution within %d iterations\n");
+endfunction
