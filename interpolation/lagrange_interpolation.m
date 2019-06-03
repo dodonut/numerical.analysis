ebd6a2de84b9c6d89b9c768a529b58e731342294
dodonut <viniciusvims@gmail.com>
2019-06-03 10:52:49 -0300

    fix lagrange

--- interpolation/lagrange_interpolation.m
+++ interpolation/lagrange_interpolation.m
@@ -1,21 +1,28 @@
-function y = lagrange_interpolation(x,px,py)
+function lagrange_interpolation(px, fx)
   %LAGRANGE_INTERPOLATION(x,px,py)
-  %x,y               are row matrices of same length which represent the 
+  %x                 the point which you want to interpolate
+  %px,py             are row matrices of same length which represent the 
   %                  datapoints to interpolate
-  if length(px) != length(py) 
-    error("vectors must have same dimentions");
-  endif
-  n = length(px);
-  L = ones(n);
+  x = px; %form a interval between the points 
+  n = length(x);
+  L = ones(n,1)';
+  yL = zeros(n,1)';
+
   for i = 1:n
+    x0 = x(i);
     for j = 1:n
       if i != j
-        L(i) *= (x - px(j))/(px(i) - px(j));
+        L(i) *= (x0 - px(j))/(px(i) - px(j));
       endif
     endfor
   endfor
-  y = 0;
+  
   for i = 1:n
-    y += py(i)*L(i);
+    yL(i) = fx(x(i))*L(i);
   endfor
+  
+  yO = fx(x);             %original function
+  plot(x,yL, 'bo;Interplado;', x, yO, 'gr;Original;');
+  title ("Comparativo da funcao original e interpolada");
+  
 endfunction
